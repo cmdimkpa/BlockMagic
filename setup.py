475ddbd14b5f91e18ace0d57412dd0128ba6d29f
Monty Dimkpa <cmdimkpa@gmail.com>
2018-06-11 11:33:48 +0100

    setup

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name = 'block_magic',
+    packages = ['block_magic'],
+    version = '1.0.1',
+    description = 'Simple Web I/O on Secure Blockchain',
+    author = 'Monty Dimkpa',
+    author_email = 'cmdimkpa@gmail.com',
+    url = 'https://github.com/cmdimkpa/BlockMagic',
+    download_url = 'https://github.com/cmdimkpa/BlockMagic/archive/v1.0.1.tar.gz',
+    keywords = ['Web I/O', 'blockchain'],
+    classifiers = [],
+)
