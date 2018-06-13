93babaa7b35d0c6f0ee9258062d2bb0913c74e38
Monty Dimkpa <cmdimkpa@gmail.com>
2018-06-13 10:10:41 +0100

    Client v2.0.2

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from distutils.core import setup
 setup(
     name = 'block_magic',
     packages = ['block_magic'],
-    version = '1.0.1',
+    version = '2.0.2',
     description = 'Simple Web I/O on Secure Blockchain',
     author = 'Monty Dimkpa',
     author_email = 'cmdimkpa@gmail.com',
     url = 'https://github.com/cmdimkpa/BlockMagic',
-    download_url = 'https://github.com/cmdimkpa/BlockMagic/archive/v1.0.1.tar.gz',
+    download_url = 'https://github.com/cmdimkpa/BlockMagic/archive/v2.0.2.tar.gz',
     keywords = ['Web I/O', 'blockchain'],
     classifiers = [],
 )
